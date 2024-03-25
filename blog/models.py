a5c1761dcfbf3b8529b37c8a7ed4781b06c015be
desokan <thefdmm@gmail.com>
2024-03-25 10:53:24 +0000

    added many-to-one relationship between post and user

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Post(models.Model):
 
@@ -8,8 +9,11 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    title = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250)
     body = models.TextField()
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
 
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
