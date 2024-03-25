90553aade56741986e8ab1122a8636a11bc470aa
desokan <thefdmm@gmail.com>
2024-03-25 09:31:05 +0000

    added sort order and index

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,17 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=250)
     title = models.SlugField(max_length=250)
-    bode = models.TextField()
+    body = models.TextField()
 
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
 
     def __str__(self):
         return self.title
