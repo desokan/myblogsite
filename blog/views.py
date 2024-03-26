2c4359ea0a22922002b023757f88893964af654b
desokan <thefdmm@gmail.com>
2024-03-26 23:14:50 +0000

    corrected wrong file name

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,9 @@ from .models import Post
 def post_list(request):
     posts = Post.published.all()
     return render(request,
-                 'blog/post/list.html',
-                 {'posts': posts})
+                  'blog/post/list.html',
+                  {'posts': posts})
+
 
 def post_detail(request, id):
     post = get_object_or_404(Post,
