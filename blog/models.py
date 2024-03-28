d20e1eb64ca0c48cfd839d4fbde5564134d5a774
desokan <thefdmm@gmail.com>
2024-03-28 17:26:57 +0000

    updated the absolute url in the model

--- blog/models.py
+++ blog/models.py
@@ -44,3 +44,10 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('blog:post_detail',
                        args=[self.id])
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
