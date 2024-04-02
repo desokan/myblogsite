d2259c674fcefb76fa590b860e6373e8e0d9a0c2
desokan <thefdmm@gmail.com>
2024-04-02 06:44:17 +0100

    replaced function-based view with class-based view.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage,\
-                                  PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, \
+    PageNotAnInteger
+from django.views.generic import ListView
+
 
 def post_list(request):
     post_list = Post.published.all()
@@ -14,8 +16,9 @@ def post_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     return render(request,
-                 'blog/post/list.html',
-                 {'posts': posts})
+                  'blog/post/list.html',
+                  {'posts': posts})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
@@ -27,3 +30,13 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
